19137d9a49710bd332eb28e1203cfe9ff9005344
lyteking <lightebube751@gmail.com>
2025-08-19 11:01:25 +0100

    Updated Landing Page Design

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,15 +8,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body>
         <header className="p-4 shadow-md flex items-center bg-background">
-          <div className="flex items-center justify-between w-full px-15">
+          <div className="flex items-center justify-between w-full md:px-8 lg:px-15">
             <Image src={logo} alt="Drid Research Logo" width={54} height={54} />
-            <div className="flex items-center text-xl text-black justify-around w-1/2">
+            <div className="hidden md:flex md:items-center lg:text-xl text-black justify-between w-1/2">
               <section>Home</section>
               <section>Pricing</section>
               <section>About Course</section>
               <section>My Classroom</section>
             </div>
-            <Image src={logo} alt="Drid Research Logo" width={54} height={54} />
+            <div className="hidden md:flex">
+              <Image src={logo} alt="Drid Research Logo" width={54} height={54} />
+            </div>
           </div>
         </header>
         <main>{children}</main>
--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,23 @@ import image from "./components/LandingPageImage.png"
 export default function Home() {
   return(
     <>
-      <div className="bg-purple-800">
-        <div className="max-w-7xl te mx-auto px-6 lg:px-12 py-16 grid lg:grid-cols-2 gap-12 items-center">
+      <div className="bg-purple-800 w-full">
+        <div className="max-w-7xl te mx-auto px-6 md:px-10 lg:px-12 py-16 grid md:grid-cols-2 lg:grid-cols-2 gap-12 items-center">
         <div className="space-y-6 col-span-1">
-          <h1 className="text-big md:text-big font-bold leading-none">
+          <h1 className="text-4xl md:text-5xl lg:text-7xl font-bold leading-none">
             Connect. <br /> 
             Learn. Grow.
           </h1>
-          <p className="text-lg text-gray-300 max-w-md">
+          <p className="text-sm font-bold text-gray-300 max-w-md">
             Advance your skills through structured research and innovation 
             courses curated by the Directorate of Research, Innovation and 
             Development (DRID), University of Benin.
           </p>
-          <button className="bg-purple-500 hover:bg-pink-700 text-white px-15 py-3 rounded-md font-semibold transition">
+          <button className="bg-purple-500 hover:bg-pink-700 text-white md:px-15 py-3 rounded-md font-semibold transition text-sm px-4">
             REGISTER NOW
           </button>
         </div>
-        <div className="col-span-1">
+        <div className="hidden md:flex col-span-1">
           <Image src={image} alt="image" width={758} height={758} />
         </div>
         </div>

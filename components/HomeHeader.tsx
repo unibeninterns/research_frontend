873ecd8961549c5019604ca11054b076ea660790
xilder <olabowaleayobami@gmail.com>
2025-08-26 04:10:45 +0200

    Chore: styled navbar for the homepage

--- components/HomeHeader.tsx
+++ components/HomeHeader.tsx
@@ -6,15 +6,15 @@ export default function Header() {
   return (
     <header className="fixed top-0 w-full left-0 z-50 p-4 shadow-md flex items-center bg-pink-100">
       <div className="flex items-center justify-between w-full md:px-8 lg:px-15">
-        <Image src={logo || "/placeholder.svg"} alt="Drid Research Logo" width={54} height={54} />
-        <div className="hidden md:flex md:items-center lg:text-xl text-black justify-between w-1/2">
-          <a href="">Home</a>
-          <a href="/pricing">Pricing</a>
+        <Image src={logo || "/placeholder.svg"} alt="Drid Research Logo" width={30} height={30} />
+        <div className="hidden md:flex md:items-center text-[16px] uppercase text-black justify-between w-1/2">
+          <Link href="/">Home</Link>
+          <Link href="/pricing">Pricing</Link>
           <Link href="/about">About Course</Link>
-          <a href="">My Classroom</a>
+          <Link href="">My Classroom</Link>
         </div>
         <div className="hidden md:flex">
-          <Image src={logo || "/placeholder.svg"} alt="Drid Research Logo" width={54} height={54} />
+          <Image src={logo || "/placeholder.svg"} alt="Drid Research Logo" width={30} height={30} />
         </div>
       </div>
     </header>

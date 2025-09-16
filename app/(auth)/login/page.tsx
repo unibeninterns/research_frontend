c614d2f9531814dd93aedf8344f4699af6de4c7e
xilder <olabowaleayobami@gmail.com>
2025-09-16 12:15:35 +0200

    Put the login page in an AuthProvider to resolve render deployment issues

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,10 @@
 import { LoginForm } from "../../../components/login-form";
+import { AuthProvider } from "@/auth-context";
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <AuthProvider>
+      <LoginForm />
+    </AuthProvider>
+  );
 }
